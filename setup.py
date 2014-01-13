47435d294e0bd93fb09ee59d2f3ad89216a43a66
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-01-13 12:54:46 +0200

    Up again.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setup(
     name = "mongoql-conv",
-    version = "0.2.0",
+    version = "0.2.1",
     url = 'https://github.com/ionelmc/python-mongoql-conv',
     download_url = '',
     license = 'BSD',
