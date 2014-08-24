0000517edf6bbf5c3695a023210da44d1516e1a9
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-08-25 02:41:58 +0300

    Make workaround for py2 and doctest limitation.

--- conftest.py
+++ conftest.py
@@ -1,3 +1,4 @@
+import sys
 try:
     from django import setup
 except ImportError:
@@ -6,7 +7,10 @@ else:
     def pytest_configure():
         setup()
 
-import sys
 from django.core.management import execute_from_command_line
 
 execute_from_command_line([sys.executable, 'syncdb', '--noinput'])
+
+if sys.version_info[0] == 2:  # shitty workaround for doctests
+    from mongoql_conv import InvalidQuery
+    InvalidQuery.__name__ = 'mongoql_conv.' + InvalidQuery.__name__
