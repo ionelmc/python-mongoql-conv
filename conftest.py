e17770ce4fe76416892c8bf91a694801d321b3ea
Ionel Cristian Mărieș <contact@ionelmc.ro>
2014-08-25 03:11:41 +0300

    Some cleanup. Skip src on when cover is disabled.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,8 @@
 import sys
+if sys.version_info[0] == 2:  # shitty workaround for doctests
+    from mongoql_conv import InvalidQuery
+    InvalidQuery.__name__ = 'mongoql_conv.' + InvalidQuery.__name__
+
 try:
     from django import setup
 except ImportError:
@@ -11,6 +15,3 @@ from django.core.management import execute_from_command_line
 
 execute_from_command_line([sys.executable, 'syncdb', '--noinput'])
 
-if sys.version_info[0] == 2:  # shitty workaround for doctests
-    from mongoql_conv import InvalidQuery
-    InvalidQuery.__name__ = 'mongoql_conv.' + InvalidQuery.__name__
